e150538038ef79d9c33407518bf845bfc3bb1544
KelinLiER <kelin.li@extendrobotics.com>
2025-02-24 17:16:24 +0000

    Update er_battery_policy.py

--- src/openpi/policies/er_battery_policy.py
+++ src/openpi/policies/er_battery_policy.py
@@ -102,7 +102,7 @@ class EROutputs(transforms.DataTransformFn):
 
     def __call__(self, data: dict) -> dict:
         # Only return the first 14 dims.
-        actions = np.asarray(data["actions"][:, :11])
+        actions = np.asarray(data["actions"][:, :7])
         return {"actions": actions}
 
 def _decode_aloha(data: dict, *, adapt_to_pi: bool = False) -> dict:
